8bf5e5c7a4078aa07dfcecd50fc37d5530c6557a
borhanshuvo <borhan015@gmail.com>
2021-06-04 00:32:31 +0600

    final project

--- components/ProductsList/products.js
+++ components/ProductsList/products.js
@@ -1,24 +1,5 @@
 export default [{
     name: 'Nikon',
-<<<<<<< HEAD
-    tagline: 'Good Product',
-    taglineCTA: 'Price : $150',
-    image: require('../../assets/images/2.png'),
-  }, {
-    name: 'Cannon',
-    tagline: 'Good Product',
-    taglineCTA: 'Price : $300',
-    image: require('../../assets/images/1.png'),
-  }, {
-    name: 'Nikon Lense',
-    tagline: 'Good Product',
-    taglineCTA: 'Price : $250',
-    image: require('../../assets/images/4.png'),
-  }, {
-    name: 'Cannon Lense',
-    tagline: 'Good Product',
-    taglineCTA: 'Price : $280',
-=======
     tagline: 'This is best for photo',
     taglineCTA: 'Price : $200',
     image: require('../../assets/images/2.png'),
@@ -36,7 +17,6 @@ export default [{
     name: 'Cannon Lense',
     tagline: 'This camera also have audio features',
     taglineCTA: 'Price : $200',
->>>>>>> origin/sagor
     image: require('../../assets/images/3.png'),
   }];
   
