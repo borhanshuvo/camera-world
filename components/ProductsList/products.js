54efa253b2609a14a4eb46a8ecd4f69318b982fc
Akhtaruzzaman <akhtar01947@gmail.com>
2021-06-03 23:47:40 +0600

    first commit

--- components/ProductsList/products.js
+++ components/ProductsList/products.js
@@ -1,22 +1,22 @@
 export default [{
     name: 'Nikon',
     tagline: 'Good Product',
-    taglineCTA: 'Price : $200',
+    taglineCTA: 'Price : $150',
     image: require('../../assets/images/2.png'),
   }, {
     name: 'Cannon',
     tagline: 'Good Product',
-    taglineCTA: 'Price : $200',
+    taglineCTA: 'Price : $300',
     image: require('../../assets/images/1.png'),
   }, {
     name: 'Nikon Lense',
     tagline: 'Good Product',
-    taglineCTA: 'Price : $200',
+    taglineCTA: 'Price : $250',
     image: require('../../assets/images/4.png'),
   }, {
     name: 'Cannon Lense',
     tagline: 'Good Product',
-    taglineCTA: 'Price : $200',
+    taglineCTA: 'Price : $280',
     image: require('../../assets/images/3.png'),
   }];
   
