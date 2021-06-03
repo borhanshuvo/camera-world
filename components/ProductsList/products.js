2ceb32b5024a8fa8343514a433f3cb7e616729a9
SAGOR <seakingmawmaw2020@gmail.com>
2021-06-03 23:48:27 +0600

    update tagline

--- components/ProductsList/products.js
+++ components/ProductsList/products.js
@@ -1,21 +1,21 @@
 export default [{
     name: 'Nikon',
-    tagline: 'Good Product',
+    tagline: 'This is best for photo',
     taglineCTA: 'Price : $200',
     image: require('../../assets/images/2.png'),
   }, {
     name: 'Cannon',
-    tagline: 'Good Product',
+    tagline: 'This is best for video',
     taglineCTA: 'Price : $200',
     image: require('../../assets/images/1.png'),
   }, {
     name: 'Nikon Lense',
-    tagline: 'Good Product',
+    tagline: 'It is good for photo and video',
     taglineCTA: 'Price : $200',
     image: require('../../assets/images/4.png'),
   }, {
     name: 'Cannon Lense',
-    tagline: 'Good Product',
+    tagline: 'This camera also have audio features',
     taglineCTA: 'Price : $200',
     image: require('../../assets/images/3.png'),
   }];
